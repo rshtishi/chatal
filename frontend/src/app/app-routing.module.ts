22801e510bf4cc07d273157804354e98270b2be6
Rando Shishi <rando.shtishi@gmail.com>
2021-02-26 21:52:18 +0100

    adding the login module

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: './login/login.module#LoginModule'
+  },
+  {
+    path: 'home',
+    loadChildren: './layout/home/home.module#HomeModule'
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
