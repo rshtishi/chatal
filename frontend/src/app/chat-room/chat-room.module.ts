d93acced572339c95a7265995d7f0aea58aa3de9
Rando Shishi <rando.shtishi@gmail.com>
2021-03-05 20:58:07 +0100

    moving messages in separated component

--- frontend/src/app/chat-room/chat-room.module.ts
+++ frontend/src/app/chat-room/chat-room.module.ts
@@ -5,10 +5,11 @@ import { ChatRoomRoutingModule } from './chat-room-routing.module';
 import { ChatRoomComponent } from './chat-room/chat-room.component';
 import { InboxChatComponent } from './chat-room/inbox-chat/inbox-chat.component';
 import { InboxSearchComponent } from './chat-room/inbox-search/inbox-search.component';
+import { MessagesComponent } from './chat-room/messages/messages.component';
 
 
 @NgModule({
-  declarations: [ChatRoomComponent, InboxChatComponent, InboxSearchComponent],
+  declarations: [ChatRoomComponent, InboxChatComponent, InboxSearchComponent, MessagesComponent],
   imports: [
     CommonModule,
     ChatRoomRoutingModule
