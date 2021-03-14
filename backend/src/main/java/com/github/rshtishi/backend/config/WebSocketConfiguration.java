12b27c4fccf3b09aea229edc058bef1597274503
Rando Shishi <rando.shtishi@gmail.com>
2021-03-14 10:35:48 +0100

    implementing security for chat

--- backend/src/main/java/com/github/rshtishi/backend/config/WebSocketConfiguration.java
+++ backend/src/main/java/com/github/rshtishi/backend/config/WebSocketConfiguration.java
@@ -1,6 +1,9 @@
 package com.github.rshtishi.backend.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -8,7 +11,10 @@ import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerCo
 
 @Configuration
 @EnableWebSocketMessageBroker
+@Order(Ordered.HIGHEST_PRECEDENCE + 99)
+@Slf4j
 public class WebSocketConfiguration implements WebSocketMessageBrokerConfigurer {
+
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
--- backend/src/main/java/com/github/rshtishi/backend/config/WebSocketSecurityConfiguration.java
+++ backend/src/main/java/com/github/rshtishi/backend/config/WebSocketSecurityConfiguration.java
@@ -1,8 +1,20 @@
 package com.github.rshtishi.backend.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.SimpMessageType;
+import org.springframework.security.config.annotation.web.messaging.MessageSecurityMetadataSourceRegistry;
 import org.springframework.security.config.annotation.web.socket.AbstractSecurityWebSocketMessageBrokerConfigurer;
 
 @Configuration
 public class WebSocketSecurityConfiguration extends AbstractSecurityWebSocketMessageBrokerConfigurer {
+
+    @Override
+    protected void configureInbound(MessageSecurityMetadataSourceRegistry messages) {
+        messages.anyMessage().authenticated();
+    }
+
+    @Override
+    protected boolean sameOriginDisabled() {
+        return true;
+    }
 }
